c8d23fadd40a7c20f5ee7d68b8d95cee100c7e92
Bilime <bilime1030@gmail.com>
2025-06-28 17:48:38 +0900

    Fix: fix backend logic

--- apps/lim-jeong-hoon/src/app/docs/page.tsx
+++ apps/lim-jeong-hoon/src/app/docs/page.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
+import 'swagger-ui-react/swagger-ui.css';
 
 const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
 
